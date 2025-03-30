4625ada0831e541b7c3efe02023af83c8372cf12
MelitaMabusela <101676899+MelitaMabusela@users.noreply.github.com>
2025-03-30 15:44:19 -0700

    Add files via upload

---
+++ CountDown.py
@@ -0,0 +1,15 @@
+import time
+#How to see many days, hours, minutes and seconds we have.(Digital Clock)
+
+my_time = int(input("Enter time in seconds:"))
+for x in range(my_time, 0, -1):    #Counting down
+    seconds = x %60
+    min = int(x/60) %60
+    hrs = int(x/3600) %24
+    days = int(x/86400)
+    print(f"{days:02}:{hrs:02}:{min:02}:{seconds:02}")
+    time.sleep(1)  # Counting down per second
+
+print("Time's Up!")
+
+
